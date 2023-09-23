aa070807144315a9abf0c145bb12ef8034e1449f
OYOOCALEX2022 <calexoyoo400@gmail.com>
2023-09-24 00:05:40 +0300

    added js code to toggle between see more and see less

--- myscripts.js
+++ myscripts.js
@@ -1,3 +1,17 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const seeMoreButton = document.getElementById("see-more-button");
+  const fullDescription = document.querySelector(".full-description");
+  
+  seeMoreButton.addEventListener("click", function () {
+    if (fullDescription.style.display === "block") {
+      fullDescription.style.display = "none";
+      seeMoreButton.textContent = "See More";
+    } else {
+      fullDescription.style.display = "block";
+      seeMoreButton.textContent = "See Less";
+    }
+  });
+});
 // Get references to the form elements
 const form = document.getElementById("payment-form");
 const cardNumber = document.getElementById("card-number");
